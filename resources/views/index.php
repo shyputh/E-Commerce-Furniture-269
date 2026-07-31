3d0d74486d55d19105c6abef7df34c5f2b194f95
Sherly Putriani <sherlyputriani269@student.uns.ac.id>
2026-07-31 11:12:31 +0700

    update tampilan landing page 2

--- resources/views/index.php
+++ resources/views/index.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Lora:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}"></head>
+    <link rel="stylesheet" href="./style.css">></head>
 <body>
 
     <!-- Navigation -->
@@ -141,6 +141,6 @@
         </div>
     </footer>
 
-    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="./script.js"></script>
 </body>
 </html>
